153288ae04de3a66b85ea214e14c1751bfc76b79
fonnycode <bf.tinydiaries@gmail.com>
2023-12-28 01:07:24 -0500

    User Experience (UX) has been improved.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPose(response) {
-  console.log("poses generated");
   new Typewriter("#pose-generator", {
     strings: response.data.answer,
     autoStart: true,
@@ -13,11 +12,15 @@ function generatePose(event) {
 
   let generatePose = document.querySelector("#user-input-generator");
   let apiKey = "9tce7490b0da29acf6b444190735fo2f";
-  let prompt = `${generatePose.value}`;
   let context =
     "You are the master of yoga. Knowledgeable in yoga poses,breathing exercise and meditation. Generate the information and suggesting only 1 result at a time. You are giving the answer with great benefits and match the user request. The answer only consist of 3 lines with basic HTML and nothing too long. Remember to always sign at the END by separating the line with <br /> and inside a <strong> element in color blue'Namaste 🙏🏻'.";
+  let prompt = `Generating information about ${generatePose.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  console.log("generate yoga poses");
+
+  let poseElements = document.querySelector("#pose-generator");
+  poseElements.classList.remove("hidden");
+  poseElements.innerHTML = `<div class="generating"> ⌛Generating information about ${generatePose.value}</div>`;
+
   axios.get(apiUrl).then(displayPose);
 }
 
