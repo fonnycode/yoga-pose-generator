da356aed7f586d23bfefd9fad1627445ef92af41
fonnycode <bf.tinydiaries@gmail.com>
2023-12-28 01:23:09 -0500

    added few details in footer and css

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePose(event) {
   let generatePose = document.querySelector("#user-input-generator");
   let apiKey = "9tce7490b0da29acf6b444190735fo2f";
   let context =
-    "You are the master of yoga. Knowledgeable in yoga poses,breathing exercise and meditation. Generate the information and suggesting only 1 result at a time. You are giving the answer with great benefits and match the user request. The answer only consist of 3 lines with basic HTML and nothing too long. Remember to always sign at the END by separating the line with <br /> and inside a <strong> element in color blue'Namaste 🙏🏻'.";
+    "You are the master of yoga. Knowledgeable in yoga poses,breathing exercise and meditation. Generate the information and suggesting only 1 result at a time. You are giving the answer with great benefits and match the user request. The answer only consist of 3 lines and NOT long. Remember to always sign at the END by separating the line with <br /> and inside a <strong> element 'Namaste 🙏🏻'.";
   let prompt = `Generating information about ${generatePose.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
